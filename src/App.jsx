9958440d833dd98cd1d150a5815c3ad6f781228e
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-17 20:31:29 +0600

    card body add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import AllBooks from "./AllBooks/AllBooks";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <AllBooks></AllBooks>
+      <Footer></Footer>
     </>
   );
 }
