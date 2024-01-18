86ea6ff9735a3685265329be3a441462d4a380aa
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-18 15:15:27 +0600

    data load and favorite unfavorite add

--- src/AllBooks/AllBooks.jsx
+++ src/AllBooks/AllBooks.jsx
@@ -1,23 +1,28 @@
-import book from "../assets/book.png";
-import star from "../assets/star.svg";
 import BooksAction from "./BooksAction";
 import BooksCard from "./BooksCard";
+import books from "../../public/books.json";
+import { useState } from "react";
 
 export default function AllBooks() {
+  const [allBooks, setAllBooks] = useState(books);
+  console.log(allBooks);
+
+  function handleFavorite(bookId) {
+    const bookIndex = allBooks.findIndex((book) => book.id === bookId);
+    const newTasks = [...allBooks];
+    newTasks[bookIndex].favorite = !newTasks[bookIndex].favorite;
+    setAllBooks(newTasks);
+  }
+
   return (
     <>
       <main className="my-10 lg:my-14">
-        {/* <!-- header --> */}
         <header className="mb-8 lg:mb-10 mx-auto max-w-7xl">
           <BooksAction></BooksAction>
         </header>
-        {/* <!-- header ends --> */}
-        {/* <!-- Book Grid --> */}
         <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {/* <!-- Book Item --> */}
-          <BooksCard></BooksCard>
+          <BooksCard books={allBooks} onFav={handleFavorite}></BooksCard>
         </div>
-        {/* <!-- Book Grid Ends --> */}
       </main>
     </>
   );
