b0e2409ec2be36376569115222c1611920bbdfbe
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-18 13:24:52 +0600

    books action and books card add

--- src/AllBooks/AllBooks.jsx
+++ src/AllBooks/AllBooks.jsx
@@ -1,5 +1,7 @@
 import book from "../assets/book.png";
 import star from "../assets/star.svg";
+import BooksAction from "./BooksAction";
+import BooksCard from "./BooksCard";
 
 export default function AllBooks() {
   return (
@@ -7,137 +9,13 @@ export default function AllBooks() {
       <main className="my-10 lg:my-14">
         {/* <!-- header --> */}
         <header className="mb-8 lg:mb-10 mx-auto max-w-7xl">
-          <div className="mx-auto flex items-end justify-between max-md:max-w-[95%] max-md:flex-col max-md:items-start max-md:space-y-4">
-            {/* <!-- info , title , search --> */}
-            <div>
-              <h6 className="mb-2 text-base lg:text-xl">Trending on 2021</h6>
-              <h2 className="mb-6 font-['Playfair_Display'] text-3xl font-bold lg:text-4xl">
-                Trending Books of the Year
-              </h2>
-              {/* <!-- Search Box --> */}
-              <form>
-                <div className="flex">
-                  <div className="relative w-full overflow-hidden rounded-lg border-2 border-[#1C4336] text-[#1C4336] md:min-w-[380px] lg:min-w-[440px]">
-                    <input
-                      type="search"
-                      id="search-dropdown"
-                      className="z-20 block w-full bg-white px-4 py-2.5 pr-10 text-[#1C4336] placeholder:text-[#1C4336] focus:outline-none"
-                      placeholder="Search Book"
-                      required
-                    />
-                    <div className="absolute right-0 top-0 flex h-full items-center">
-                      <button
-                        type="submit"
-                        className="mr-1.5 flex items-center space-x-1.5 rounded-md rounded-e-lg bg-[#1C4336] px-4 py-2.5 text-sm text-white"
-                      >
-                        <svg
-                          className="h-[14px] w-[14px]"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                          />
-                        </svg>
-                        <span>Search</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-              {/* <!-- Search Box Ends --> */}
-            </div>
-            {/* <!-- sort - filter --> */}
-            <div className="flex items-stretch space-x-3">
-              {/* <!-- Sort --> */}
-              <select
-                className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
-                name="sortBy"
-                id="sortBy"
-              >
-                <option value="">Sort</option>
-                <option value="name_asc">Name (A-Z)</option>
-                <option value="name_desc">Name (Z-A)</option>
-                <option value="year_asc">Publication Year (Oldest)</option>
-                <option value="year_desc">Publication Year (Newest)</option>
-              </select>
-            </div>
-          </div>
+          <BooksAction></BooksAction>
         </header>
         {/* <!-- header ends --> */}
         {/* <!-- Book Grid --> */}
         <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {/* <!-- Book Item --> */}
-          <div className="space-y-3">
-            {/* <!-- thumbnail --> */}
-            <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4">
-              <img className="max-w-[144px]" src={book} alt="book name" />
-            </div>
-            {/* <!-- info --> */}
-            <div className="space-y-3">
-              <h4 className="text-lg font-bold lg:text-xl">
-                JavaScript and Jquery
-              </h4>
-              <p className="text-xs lg:text-sm">
-                By : <span>Jon Duckett</span>
-              </p>
-              <div className="flex items-center justify-between">
-                <h4 className="text-lg font-bold lg:text-xl">$62</h4>
-                {/* <!-- stars --> */}
-                <div className="flex items-center space-x-1">
-                  <img src={star} />
-                  <img src={star} />
-                  <img src={star} />
-                  <img src={star} />
-                  <span className="text-xs lg:text-sm">(4 Star)</span>
-                </div>
-                {/* <!-- stars ends --> */}
-              </div>
-
-              <div className="flex items-center gap-3 text-xs lg:text-sm">
-                <button className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336] py-1.5 text-white transition-all hover:opacity-80 lg:py-1.5">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-5 w-5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                    />
-                  </svg>
-                  Add to Cart
-                </button>
-                <button className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%] lg:py-1.5">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-5 w-5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
-                  </svg>
-                  Favourite
-                </button>
-              </div>
-            </div>
-          </div>
+          <BooksCard></BooksCard>
         </div>
         {/* <!-- Book Grid Ends --> */}
       </main>
